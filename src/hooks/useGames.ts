2a67441dc42fa5f55693076f1b869befa44a7f3c
tdiop <tdp@tinubu.com>
2023-10-18 11:11:40 +0200

    Show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,23 +24,30 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState();
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", {
         signal: controller.signal,
       })
-      .then(({ data: { results } }) => setGames(results))
+      .then(({ data: { results } }) => {
+        setGames(results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
