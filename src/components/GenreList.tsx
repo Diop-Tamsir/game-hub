801119017f1c4bef66a56c2340be1fd925c2a22e
tdiop <tdp@tinubu.com>
2023-11-19 19:33:50 +0100

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,17 +23,23 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 
   return (
     <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       {error && <Text>{error}</Text>}
       <List>
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY={1}>
             <HStack>
               <Image
+                objectFit="cover"
                 borderRadius={6}
                 src={getCroppedImageUrl(genre.image_background)}
                 boxSize="30px"
               />
               <Button
+                whiteSpace="normal"
+                textAlign="left"
                 fontWeight={genre.id == selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
